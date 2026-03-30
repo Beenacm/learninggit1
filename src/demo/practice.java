8b0c735b75464bad06ec87ffcbb14688bbe70a01
beena <cmbeena1@gmail.com>
2026-03-30 13:07:30 +0530

    second commit from git

--- src/demo/practice.java
+++ src/demo/practice.java
@@ -5,6 +5,7 @@ public class practice {
 	public static void main(String[] args) {
 	System.out.println("hi");
 	System.out.println("hi from feature branch");
+	System.out.println("once again hi from feature branch");
 
 	}
 
